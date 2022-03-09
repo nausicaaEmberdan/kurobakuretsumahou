0a25b200060c7d37adfc63c023c2162f6d0dde04
nausicaaEmberdan <101249789+nausicaaEmberdan@users.noreply.github.com>
2022-03-09 20:53:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Dũng giả thân yêu à!',
+    introDesc: `Nghe theo tiếng gọi lý trí
+    Thân người hiền, tránh người ác
+    Biết ta biết bạn`,
+    btnIntro: 'Liệu người đã sẵn sàng',
+    title: 'Câu phép Hắc Bộc Phát Ma Thuật',
+    desc: 'Chọn đúng hoặc bốc cứt ',
+    btnYes: 'Phạm Gia Hưng là chó sủa gâu gâu và thích ăn cwts',
+    btnNo: 'Phạm Gia Hưng là nhất',
+    question:'Thấy tôi đỉnh không bạn ;)',
+    btnReply: 'Nộp phiếu cho hội thám hiểm',
+    reply: 'Đỉnh vãi cả lol',
+    mess: 'T cũng biết thế mà đỉnh quá đỉnh quá',
+    messDesc: 'Ỏooooo cute quá đáng iu quá trời ơi người gì đâu như con cặc',
+    messLink: 'https://drive.google.com/file/d/1QWbUmSZOEvEAzzKu--szpe0KWKXPRhJD/view?usp=sharing' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
